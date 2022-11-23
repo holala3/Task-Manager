5fa66a54bf84e9301036ed095b6d1b69b720194c
Jacobo giraldo <jacobitogiraldo3@gmail.com>
2022-11-23 11:47:29 -0500

    mas cosas02

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch(){
+    //Creamos un estado con useState
+    const [searchValue, setSearchValue] = React.useState("");
+    //Tenemos una funcion para alterar este estado llamada setSearchValue
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     };
 
-    return(
-        <input placeholder='Buscar tarea' className="TodoSearch-input"
+    return[
+        <input 
+        placeholder='Buscar tarea' 
+        className="TodoSearch-input"
+        value={searchValue}
         onChange={onSearchValueChange}
-        ></input>
-    )
+        ></input>,
+        
+        <p>{searchValue}</p>
+    ]
 }
 
 export { TodoSearch }
