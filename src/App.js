78e60a799cd37e28461a2b11b974b00d504d299f
Jacobo giraldo <jacobitogiraldo3@gmail.com>
2022-11-23 18:43:52 -0500

    mas cambios02

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { CreateTodoButton } from "./CreateTodoButton";
 import { TodoTittle } from "./TodoTittle";
 import './App.css';
 
-const todos=[
+const defaultTodos=[
   {text:'Tomar agua', completed: false },
   {text:'Tomar curso de NodeJs', completed: false },
   {text:'Tomar curso de sql', completed: false }
@@ -16,11 +16,24 @@ const todos=[
 
 
 function App(){
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+    //Creamos un estado con useState
+  //Tenemos una funcion para alterar este estado llamada setSearchValue
   return (
     <React.Fragment>
       <TodoTittle/>
-      <TodoSearch />
-      <TodoCounter />
+      <TodoSearch 
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      />
+      <TodoCounter 
+      total={totalTodos}
+      completed={completedTodos}
+      />
 
     
     
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,26 +1,20 @@
 import React from "react";
 import "./TodoSearch.css"
 
-function TodoSearch(){
-    //Creamos un estado con useState
-    const [searchValue, setSearchValue] = React.useState("");
-    //Tenemos una funcion para alterar este estado llamada setSearchValue
-
+function TodoSearch({ searchValue, setSearchValue }){
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value)
     };
 
-    return[
+    return(
         <input 
         placeholder='Buscar tarea' 
         className="TodoSearch-input"
         value={searchValue}
         onChange={onSearchValueChange}
-        ></input>,
-        
-        <p>{searchValue}</p>
-    ]
+        ></input>
+    )
 }
 
 export { TodoSearch }
